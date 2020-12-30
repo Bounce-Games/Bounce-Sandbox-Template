5430a171aef176409345b5585b9821db9f8b098a
NafeeJ <nafeejan2@gmail.com>
2020-12-30 00:58:03 -0500

    Templatized from Holo Bounce v1.0

--- Assets/Scripts/PauseMenuController.cs
+++ Assets/Scripts/PauseMenuController.cs
@@ -1,19 +1,28 @@
 ﻿using UnityEngine;
+using UnityEngine.Rendering;
+using TMPro;
+using System.Collections.Generic;
 
 public class PauseMenuController : MonoBehaviour
 {
     [SerializeField] private GameObject pauseMenu = null;
+    [SerializeField] private TMP_Dropdown dropdown = null;
 
-    [SerializeField] private GameObject one = null;
-    [SerializeField] private GameObject two = null;
-    [SerializeField] private GameObject three = null;
-
-    private bool gamePaused;
     private SpawnObject mySpawnObj;
 
     void Start()
     {
         mySpawnObj = FindObjectOfType<SpawnObject>();
+
+        //Adding objects to dropdown menu
+        List<string> options = new List<string>();
+        options.Add("Random");
+        foreach(GameObject obj in SceneController.objs)
+        {
+            options.Add(obj.name);
+        }
+        AddDropdownOptions(dropdown, options);
+
         Resume();
     }
 
@@ -21,7 +30,7 @@ public class PauseMenuController : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
-            switch (gamePaused)
+            switch (SceneController.gamePaused)
             {
                 case true:
                     Resume();
@@ -33,42 +42,43 @@ public class PauseMenuController : MonoBehaviour
         }
     }
 
+    //Resume() and Pause() work by switching timeScale and active state of Pause Menu.
+    //OnDemandRendering used to create better performance by skipping every other frame in pause menu.
+
     public void Resume()
     {
+        OnDemandRendering.renderFrameInterval = 1;
         Time.timeScale = 1f;
         pauseMenu.SetActive(false);
         pauseMenu.transform.Find("ResumeButton").transform.localScale = new Vector3(1f, 1f, 1f);
-        gamePaused = false;
+        SceneController.gamePaused = false;
     }
 
     public void Pause()
     {
+        OnDemandRendering.renderFrameInterval = 2;
         Time.timeScale = 0f;
         pauseMenu.SetActive(true);
-        gamePaused = true;
+        SceneController.gamePaused = true;
     }
 
+    public void AddDropdownOptions(TMP_Dropdown dropdown, List<string> options)
+    {
+        dropdown.ClearOptions();
+        dropdown.AddOptions(options);
+    }
+
+    //Set up in dropdown of the Unity Editor. If the selection is 0, the user chose random so tells SpawnObject to make spawnRandom true and returns. 
+    //Otherwise, sets the objToSpawn to be the the object chosen using the index of the dropdown.
     public void SelectObj(int selection)
     {
-        if (selection != 0)
-        {
-            mySpawnObj.spawnRandom = false;
-        }
+        mySpawnObj.spawnRandom = selection == 0;
+        if (selection == 0) return;
+        mySpawnObj.objToSpawn = SceneController.objs[selection - 1];
+    }
 
-        switch (selection)
-        {
-            case 0:
-                mySpawnObj.spawnRandom = true;
-                break;
-            case 1:
-                mySpawnObj.objToSpawn = one;
-                break;
-            case 2:
-                mySpawnObj.objToSpawn = two;
-                break;
-            case 3:
-                mySpawnObj.objToSpawn = three;
-                break;
-        }
+    public void OpenGitHub()
+    {
+        Application.OpenURL(""); //GitHub repo link here
     }
 }
--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -1,24 +1,68 @@
 ﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using System.Collections.Generic;
 
 public class SceneController : MonoBehaviour
 {
     private SpawnObject mySpawnObj;
 
+    //Array of objects to bounce
+    public static GameObject[] objs;
+    //Scales of objects
+    public static float[] objSizes;
+    public static bool gamePaused;
+    public static bool mouseOverObject;
+
     void Start()
     {
-        //QualitySettings.vSyncCount = 1;
+        QualitySettings.vSyncCount = 0;
+        Application.targetFrameRate = Screen.currentResolution.refreshRate;
+
         mySpawnObj = FindObjectOfType<SpawnObject>();
+
+        objs = Resources.LoadAll<GameObject>(""); //INSERT PATH OF OBJECT PREFABS;
+        objSizes = new float[objs.Length];
+        //Hardcode scales of objects at each index here
     }
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.F))
+        if (!gamePaused)
         {
-            mySpawnObj.InstantiateObject();
+            if (Input.GetKeyDown(KeyCode.F))
+            {
+                mySpawnObj.positionToSpawn = new Vector2(0, 0);
+                mySpawnObj.InstantiateObject();
+            }
+            else if (Input.GetMouseButtonDown(0) && !mouseOverObject && !IsPointerOverUIObject())
+            {
+                mySpawnObj.positionToSpawn = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                mySpawnObj.InstantiateObject();
+            }
+            
         }
-        if (Input.GetKeyDown(KeyCode.C))
+
+        if (Input.GetKeyDown(KeyCode.C) && !gamePaused)
         {
             mySpawnObj.Clear();
         }
+    }
+
+    private void OnApplicationQuit()
+    {
+        //Resets prefab scales
+        for (int i = 0; i < objSizes.Length; i++)
+        {
+            objs[i].transform.localScale = new Vector3(objSizes[i], objSizes[i], 0f);
+        }
+    }
+
+    public static bool IsPointerOverUIObject()
+    {
+        PointerEventData eventDataCurrentPosition = new PointerEventData(EventSystem.current);
+        eventDataCurrentPosition.position = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        List<RaycastResult> results = new List<RaycastResult>();
+        EventSystem.current.RaycastAll(eventDataCurrentPosition, results);
+        return results.Count > 0;
     }
 }
